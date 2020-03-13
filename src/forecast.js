c9b1f8139eaea3eed174b3b8e7b18ca9d25f53cf
unknown <olek130a@gmail.com>
2020-03-13 12:40:41 +0100

    Added pressure to the forceast

--- src/forecast.js
+++ src/forecast.js
@@ -8,7 +8,7 @@ const forecast = (longitude,latitude,callback) => {
         } else if(body.error) {
             callback('Unable to find location', undefined)
         }else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The pressure is ' + body.currently.pressure + ' hPa')
         }
     })
 }
